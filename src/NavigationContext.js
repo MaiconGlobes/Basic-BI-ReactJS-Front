82a37683efd17b63f2cb7777c013a20caaa83a65
MaiconGlobes <maico-globes@hotmail.com>
2024-03-22 22:34:22 -0300

    Commit

---
+++ src/NavigationContext.js
@@ -0,0 +1,27 @@
+import { createContext, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const NavigationContext = createContext();
+
+export const useNavigation = () => useContext(NavigationContext);
+
+export const NavigationProvider = ({ children }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (path) => {
+      navigate(path);
+  };
+
+  return (
+      <NavigationContext.Provider value={{ navigate: handleNavigate }}>
+         {children}
+      </NavigationContext.Provider>
+  );
+};
+
+// export const handleNavigate = (path) => {
+//    throw new Error('Cannot call handleNavigate outside of NavigationContext');
+// };
+
+
+export const handleNavigate = (path) => {};
