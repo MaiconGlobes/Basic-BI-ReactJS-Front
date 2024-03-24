3b5d46d59200a778f571c3dc69fd23e6501e997a
MaiconGlobes <maico-globes@hotmail.com>
2024-03-24 01:41:46 -0300

    commit

--- src/pages/extra-pages/EditProfile.js
+++ src/pages/extra-pages/EditProfile.js
@@ -4,8 +4,15 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import CircularProgressWithLabel from '../../components/@extended/CircularWithValueLabel';
-import { Typography } from '@mui/material';
+import { Typography, Link } from '@mui/material';
 import MainCard from 'components/MainCard';
+import { MoreOutlined } from '@ant-design/icons';
+import Avatar from '@mui/material/Avatar';
+import { deepOrange } from '@mui/material/colors';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Stack from '@mui/material/Stack';
 
 const EditProfilePage = () => (
   <>
@@ -48,11 +55,56 @@ const EditProfilePage = () => (
               </Box>
             </Box>
           </Grid>
-          {/* Segunda coluna para o Botão, que muda para uma nova linha em telas pequenas */}
           <Grid item xs={12} sm={'auto'}>
             <Button variant="contained" color="primary" fullWidth>
               Edit your profile
             </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+
+      <Paper
+        elevation={0}
+        sx={{
+          padding: '15px'
+        }}
+      >
+        <Grid container spacing={2} alignItems="center" justifyContent="left">
+          <Grid item md={4} sm={12}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2
+                //  background: 'blue'
+              }}
+            >
+              
+LEFT
+
+            </Box>
+          </Grid>
+          <Grid item md={8} sm={12}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2
+                //  background: 'green'
+              }}
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  // background: 'red',
+                  gap: 2
+                }}
+              >
+                
+RIGTH
+              </Box>
+            </Box>
           </Grid>
         </Grid>
       </Paper>
